857e252f87280e3c2a7d09ef9602bd3fd4294447
Jey <fajarnurmulyana@gmail.com>
2026-08-28 08:11:44 +0700

    tambahan dong pun

---
+++ KilogramOrLbs.py
@@ -0,0 +1,37 @@
+def timbangan():
+    lbs = 2.20462
+
+    while True:
+        pilihan = input(
+            "\nPilih menu:\n"
+            "K = Kilogram\n"
+            "L = Pound\n"
+            "Q = Keluar\n"
+            "Pilihan: "
+        ).casefold()
+
+        if pilihan == "q":
+            print("Kamu keluar!")
+            break
+
+        elif pilihan == "k":
+            try:
+                berat = float(input("Masukkan berat badan dalam KG: "))
+                print(f"Berat anda adalah {berat} Kilogram")
+
+            except ValueError:
+                print("Masukkan angka yang valid!")
+
+        elif pilihan == "l":
+            try:
+                berat = float(input("Masukkan berat badan dalam KG: "))
+                print(f"Berat anda adalah {berat * lbs:.2f} Pound")
+
+            except ValueError:
+                print("Masukkan angka yang valid!")
+
+        else:
+            print("Pilihan tidak valid!")
+
+
+timbangan()
